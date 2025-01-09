b8aba74878761f0903dd9b729dec6bf74c3fb641
sathwika1898 <mannem.sathwika18@gmail.com>
2025-01-08 22:33:32 -0500

    predict_pipeline

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import dill
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
 
 from src.exception import CustomException
 
@@ -28,6 +29,12 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            #para=param[list(models.keys())[i]]
+
+            #gs = RandomizedSearchCV(model, para, cv=3, n_iter=50, random_state=42)
+            #gs.fit(X_train,y_train)
+
+            #model.set_params(**gs.best_params_)
 
             model.fit(X_train,y_train)
 
@@ -42,6 +49,15 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
